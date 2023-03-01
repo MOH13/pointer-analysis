5c487b1aa0f2cdc574102a70397f21634c75d674
Lasse Møldrup <lasse.moeldrup@gmail.com>
2023-03-01 18:08:50 +0100

    Refactor constraint generation to use a new IR

--- src/analysis.rs
+++ src/analysis.rs
@@ -1,34 +1,29 @@
 use std::fmt::{self, Debug, Display, Formatter};
 
-use either::Either;
 use hashbrown::HashMap;
-use llvm_ir::function::ParameterAttribute;
-use llvm_ir::instruction::{
-    AddrSpaceCast, Alloca, BitCast, Call, InlineAssembly, Load, Phi, Store,
-};
-use llvm_ir::terminator::{Invoke, Ret};
-use llvm_ir::types::Type;
-use llvm_ir::{Constant, Function, Instruction, Module, Name, Operand, Terminator};
+use llvm_ir::{Module, Name};
 
-use crate::module_visitor::{Context, ModuleVisitor};
+use crate::module_visitor::{
+    ModuleVisitor, PointerContext, PointerInstruction, PointerModuleVisitor,
+};
 use crate::solver::{Constraint, Solver};
 
 pub struct PointsToAnalysis;
 
 impl PointsToAnalysis {
     /// Runs the points-to analysis on LLVM module `module` using `solver`.
-    pub fn run<S>(module: &Module) -> PointsToResult<S>
+    pub fn run<'a, S>(module: &'a Module) -> PointsToResult<S>
     where
-        S: Solver<Term = Cell>,
+        S: Solver<Term = Cell<'a>> + 'a,
     {
-        let mut cell_finder = PointsToPreAnalyzer::new();
-        cell_finder.visit_module(module);
-        let cells_copy = cell_finder.cells.clone();
+        let mut pre_analyzer = PointsToPreAnalyzer::new();
+        pre_analyzer.visit_module(module);
+        let cells_copy = pre_analyzer.cells.clone();
 
-        let solver = S::new(cell_finder.cells);
+        let solver = S::new(pre_analyzer.cells);
         let mut points_to_solver = PointsToSolver {
             solver,
-            summaries: cell_finder.summaries,
+            summaries: pre_analyzer.summaries,
         };
         points_to_solver.visit_module(module);
 
@@ -44,9 +39,9 @@ impl PointsToAnalysis {
     }
 }
 
-pub struct PointsToResult<S: Solver>(HashMap<Cell, S::TermSet>);
+pub struct PointsToResult<'a, S: Solver>(HashMap<Cell<'a>, S::TermSet>);
 
-impl<S> Display for PointsToResult<S>
+impl<'a, S> Display for PointsToResult<'a, S>
 where
     S: Solver,
     S::TermSet: fmt::Debug,
@@ -59,15 +54,36 @@ where
     }
 }
 
-#[derive(Clone, Hash, PartialEq, Eq)]
-pub enum Cell {
-    Local { reg_name: Name, fun_name: String },
+#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+pub enum Cell<'a> {
+    Local {
+        reg_name: &'a Name,
+        fun_name: &'a str,
+    },
     // Since LLVM is in SSA, we can use the name of the allocation register to refer to the allocation site
-    Stack { reg_name: Name, fun_name: String },
-    Heap { reg_name: Name, fun_name: String },
+    Stack {
+        reg_name: &'a Name,
+        fun_name: &'a str,
+    },
+    Heap {
+        reg_name: &'a Name,
+        fun_name: &'a str,
+    },
 }
 
-impl Debug for Cell {
+impl<'a> Cell<'a> {
+    fn new_local(reg_name: &'a Name, fun_name: &'a str) -> Self {
+        Self::Local { reg_name, fun_name }
+    }
+    fn new_stack(reg_name: &'a Name, fun_name: &'a str) -> Self {
+        Self::Stack { reg_name, fun_name }
+    }
+    fn new_heap(reg_name: &'a Name, fun_name: &'a str) -> Self {
+        Self::Heap { reg_name, fun_name }
+    }
+}
+
+impl<'a> Debug for Cell<'a> {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         match self {
             Self::Local { reg_name, fun_name } => write!(f, "{reg_name}@{fun_name}"),
@@ -77,43 +93,20 @@ impl Debug for Cell {
     }
 }
 
-impl Cell {
-    fn new_local(reg_name: &Name, Context { function, .. }: Context) -> Self {
-        Cell::Local {
-            reg_name: reg_name.clone(),
-            fun_name: function.name.clone(),
-        }
-    }
-
-    fn new_stack(reg_name: &Name, Context { function, .. }: Context) -> Self {
-        Cell::Stack {
-            reg_name: reg_name.clone(),
-            fun_name: function.name.clone(),
-        }
-    }
-
-    fn new_heap(reg_name: &Name, Context { function, .. }: Context) -> Self {
-        Cell::Heap {
-            reg_name: reg_name.clone(),
-            fun_name: function.name.clone(),
-        }
-    }
-}
-
-struct FunctionSummary {
-    params: Vec<Name>,
-    return_reg: Option<Name>,
+struct FunctionSummary<'a> {
+    parameters: Vec<&'a Name>,
+    return_reg: Option<&'a Name>,
 }
 
 /// Visits a module and finds all cells in that module.
 /// An allocation site abstraction is used for heap allocations.
 /// TODO: Field sensitivity?
-struct PointsToPreAnalyzer {
-    cells: Vec<Cell>,
-    summaries: HashMap<String, FunctionSummary>,
+struct PointsToPreAnalyzer<'a> {
+    cells: Vec<Cell<'a>>,
+    summaries: HashMap<&'a str, FunctionSummary<'a>>,
 }
 
-impl PointsToPreAnalyzer {
+impl<'a> PointsToPreAnalyzer<'a> {
     fn new() -> Self {
         Self {
             cells: vec![],
@@ -121,312 +114,155 @@ impl PointsToPreAnalyzer {
         }
     }
 
-    fn add_local(&mut self, reg_name: &Name, context: Context) {
-        self.cells.push(Cell::new_local(reg_name, context));
+    fn add_local(&mut self, reg_name: &'a Name, fun_name: &'a str) {
+        self.cells.push(Cell::Local { reg_name, fun_name });
     }
 
-    fn add_stack(&mut self, reg_name: &Name, context: Context) {
-        self.cells.push(Cell::new_stack(reg_name, context));
+    fn add_stack(&mut self, reg_name: &'a Name, fun_name: &'a str) {
+        self.cells.push(Cell::Stack { reg_name, fun_name });
     }
 
-    fn add_heap(&mut self, reg_name: &Name, context: Context) {
-        self.cells.push(Cell::new_heap(reg_name, context));
+    fn add_heap(&mut self, reg_name: &'a Name, fun_name: &'a str) {
+        self.cells.push(Cell::Heap { reg_name, fun_name });
     }
 }
 
-impl ModuleVisitor for PointsToPreAnalyzer {
-    fn handle_function(&mut self, function: &Function, _module: &Module) {
-        let mut params = Vec::with_capacity(function.parameters.len());
-        for param in &function.parameters {
-            params.push(param.name.clone());
+impl<'a> PointerModuleVisitor<'a> for PointsToPreAnalyzer<'a> {
+    fn handle_ptr_function(&mut self, name: &'a str, parameters: Vec<&'a Name>) {
+        for &param in &parameters {
             self.cells.push(Cell::Local {
-                reg_name: param.name.clone(),
-                fun_name: function.name.clone(),
+                reg_name: param,
+                fun_name: name,
             });
         }
         let summary = FunctionSummary {
-            params,
+            parameters,
             return_reg: None,
         };
-        self.summaries.insert(function.name.clone(), summary);
+        self.summaries.insert(name, summary);
     }
 
-    fn handle_instruction(&mut self, instr: &Instruction, context: Context) {
+    fn handle_ptr_instruction(
+        &mut self,
+        instr: PointerInstruction<'a>,
+        context: PointerContext<'a>,
+    ) {
         match instr {
-            // Instruction::ExtractElement(_) => todo!(),
-            // Instruction::ExtractValue(_) => todo!(),
-            Instruction::Alloca(Alloca { dest, .. }) => {
-                self.add_local(dest, context);
-                self.add_stack(dest, context);
-            }
-
-            Instruction::Load(Load {
-                address: Operand::LocalOperand { ty, .. },
-                dest,
-                ..
-            }) => {
-                let pointee_type = match ty.as_ref() {
-                    Type::PointerType { pointee_type, .. } => pointee_type,
-                    _ => return,
-                };
-                if !matches!(pointee_type.as_ref(), Type::PointerType { .. }) {
-                    return;
-                }
-
-                self.add_local(dest, context);
-            }
-
-            // Instruction::GetElementPtr(_) => todo!(),
-            // Instruction::IntToPtr(_) => todo!(),
-            Instruction::BitCast(BitCast { to_type, dest, .. })
-            | Instruction::AddrSpaceCast(AddrSpaceCast { to_type, dest, .. })
-            | Instruction::Phi(Phi { to_type, dest, .. }) => {
-                if !matches!(to_type.as_ref(), Type::PointerType { .. }) {
-                    return;
-                }
-                self.add_local(dest, context);
-            }
-
-            // Instruction::Select(_) => todo!(),
-            // Instruction::Freeze(_) => todo!(),
-            Instruction::Call(Call {
+            PointerInstruction::Assign { dest, .. }
+            | PointerInstruction::Load { dest, .. }
+            | PointerInstruction::Phi { dest, .. }
+            | PointerInstruction::Call {
                 dest: Some(dest), ..
-            }) => {
-                self.add_local(dest, context);
+            } => self.add_local(dest, context.fun_name),
+
+            PointerInstruction::Alloca { dest } => {
+                self.add_local(dest, context.fun_name);
+                self.add_stack(dest, context.fun_name)
             }
-            // Instruction::VAArg(_) => todo!(),
+
+            PointerInstruction::Return { return_reg } => {
+                self.summaries.entry_ref(&context.fun_name).and_modify(|s| {
+                    s.return_reg = Some(return_reg);
+                });
+            }
+
             _ => {}
-        }
-    }
-
-    fn handle_terminator(&mut self, term: &Terminator, Context { function, .. }: Context) {
-        if let Terminator::Ret(Ret {
-            return_operand:
-                Some(Operand::LocalOperand {
-                    name: ret_name,
-                    ty: ret_ty,
-                }),
-            ..
-        }) = term
-        {
-            if !matches!(ret_ty.as_ref(), Type::PointerType { .. }) {
-                return;
-            }
-
-            self.summaries.entry_ref(&function.name).and_modify(|s| {
-                s.return_reg = Some(ret_name.clone());
-            });
         }
     }
 }
 
 /// Visits a module, generating and solving constraints using a supplied constraint solver.
-struct PointsToSolver<S> {
+struct PointsToSolver<'a, S> {
     solver: S,
-    summaries: HashMap<String, FunctionSummary>,
+    summaries: HashMap<&'a str, FunctionSummary<'a>>,
 }
 
-impl<S> PointsToSolver<S>
+impl<'a, S> PointerModuleVisitor<'a> for PointsToSolver<'a, S>
 where
-    S: Solver<Term = Cell>,
+    S: Solver<Term = Cell<'a>>,
 {
-    fn handle_call(
+    fn handle_ptr_function(&mut self, _name: &str, _parameters: Vec<&Name>) {}
+
+    fn handle_ptr_instruction(
         &mut self,
-        function: &Either<InlineAssembly, Operand>,
-        arguments: &[(Operand, Vec<ParameterAttribute>)],
-        dest: Option<&Name>,
-        context: Context,
+        instr: PointerInstruction<'a>,
+        context: PointerContext<'a>,
     ) {
-        let fun_name = match function {
-            Either::Right(Operand::ConstantOperand(name_ref)) => match name_ref.as_ref() {
-                Constant::GlobalReference {
-                    name: Name::Name(name),
-                    ..
-                } => &**name,
-                _ => return,
-            },
-            _ => return,
-        };
-
-        let summary = &self.summaries[fun_name];
-        for (arg, param) in
-            arguments
-                .iter()
-                .zip(&summary.params)
-                .filter_map(|((a, _), p)| match a {
-                    Operand::LocalOperand { name, ty }
-                        if matches!(ty.as_ref(), Type::PointerType { .. }) =>
-                    {
-                        Some((name, p))
-                    }
-                    _ => None,
-                })
-        {
-            let c = Constraint::Subset {
-                left: Cell::new_local(arg, context),
-                right: Cell::Local {
-                    reg_name: param.clone(),
-                    fun_name: fun_name.clone(),
-                },
-            };
-            self.solver.add_constraint(c);
-        }
-
-        match (&summary.return_reg, dest) {
-            (Some(return_name), Some(dest_name)) => {
-                let c = Constraint::Subset {
-                    left: Cell::Local {
-                        reg_name: return_name.clone(),
-                        fun_name: fun_name.clone(),
-                    },
-                    right: Cell::new_local(dest_name, context),
-                };
-                self.solver.add_constraint(c);
-            }
-            _ => {}
-        }
-    }
-}
-
-impl<S> ModuleVisitor for PointsToSolver<S>
-where
-    S: Solver<Term = Cell>,
-{
-    fn handle_function(&mut self, _function: &Function, _module: &Module) {}
-
-    fn handle_instruction(&mut self, instr: &Instruction, context: Context) {
         match instr {
-            // x = alloca ..
-            Instruction::Alloca(Alloca { dest, .. }) => {
-                let c = Constraint::Inclusion {
-                    term: Cell::new_stack(dest, context),
-                    node: Cell::new_local(dest, context),
-                };
-                self.solver.add_constraint(c);
-            }
-
-            // x = bitcast y to T*
-            Instruction::BitCast(BitCast {
-                operand:
-                    Operand::LocalOperand {
-                        name: value_name, ..
-                    },
-                to_type,
-                dest,
-                ..
-            })
-            | Instruction::AddrSpaceCast(AddrSpaceCast {
-                operand:
-                    Operand::LocalOperand {
-                        name: value_name, ..
-                    },
-                to_type,
-                dest,
-                ..
-            }) => {
-                if !matches!(to_type.as_ref(), Type::PointerType { .. }) {
-                    return;
-                }
+            PointerInstruction::Assign { dest, value } => {
                 let c = Constraint::Subset {
-                    left: Cell::new_local(value_name, context),
-                    right: Cell::new_local(dest, context),
+                    left: Cell::new_local(value, context.fun_name),
+                    right: Cell::new_local(dest, context.fun_name),
                 };
                 self.solver.add_constraint(c);
             }
 
-            // x = \phi(y1, y2, ..)
-            Instruction::Phi(Phi {
-                incoming_values,
-                dest,
-                to_type,
-                ..
-            }) => {
-                if !matches!(to_type.as_ref(), Type::PointerType { .. }) {
-                    return;
-                }
-                for (value, _) in incoming_values {
-                    let value_name = match value {
-                        Operand::LocalOperand { name, .. } => name,
-                        _ => continue,
-                    };
+            PointerInstruction::Store { address, value } => {
+                let c = Constraint::UnivCondSubsetRight {
+                    cond_node: Cell::new_local(address, context.fun_name),
+                    left: Cell::new_local(value, context.fun_name),
+                };
+                self.solver.add_constraint(c);
+            }
 
+            PointerInstruction::Load { dest, address } => {
+                let c = Constraint::UnivCondSubsetLeft {
+                    cond_node: Cell::new_local(address, context.fun_name),
+                    right: Cell::new_local(dest, context.fun_name),
+                };
+                self.solver.add_constraint(c);
+            }
+
+            PointerInstruction::Alloca { dest } => {
+                let c = Constraint::Inclusion {
+                    term: Cell::new_stack(dest, context.fun_name),
+                    node: Cell::new_local(dest, context.fun_name),
+                };
+                self.solver.add_constraint(c);
+            }
+
+            PointerInstruction::Phi {
+                dest,
+                incoming_values,
+            } => {
+                for value in incoming_values {
                     let c = Constraint::Subset {
-                        left: Cell::new_local(value_name, context),
-                        right: Cell::new_local(dest, context),
+                        left: Cell::new_local(value, context.fun_name),
+                        right: Cell::new_local(dest, context.fun_name),
                     };
                     self.solver.add_constraint(c);
                 }
             }
 
-            // x = *y
-            Instruction::Load(Load {
-                address:
-                    Operand::LocalOperand {
-                        name: addr_name,
-                        ty,
-                    },
+            PointerInstruction::Call {
                 dest,
-                ..
-            }) => {
-                let pointee_type = match ty.as_ref() {
-                    Type::PointerType { pointee_type, .. } => pointee_type,
-                    _ => return,
-                };
-                if !matches!(pointee_type.as_ref(), Type::PointerType { .. }) {
-                    return;
-                }
-
-                // TODO: Get rid of clones
-                let c = Constraint::UnivCondSubsetLeft {
-                    cond_node: Cell::new_local(addr_name, context),
-                    right: Cell::new_local(dest, context),
-                };
-                self.solver.add_constraint(c);
-            }
-
-            // *x = y
-            Instruction::Store(Store {
-                address:
-                    Operand::LocalOperand {
-                        name: addr_name, ..
-                    },
-                value:
-                    Operand::LocalOperand {
-                        name: value_name,
-                        ty,
-                    },
-                ..
-            }) => {
-                if !matches!(ty.as_ref(), Type::PointerType { .. }) {
-                    return;
-                }
-                let c = Constraint::UnivCondSubsetRight {
-                    cond_node: Cell::new_local(addr_name, context),
-                    left: Cell::new_local(value_name, context),
-                };
-                self.solver.add_constraint(c);
-            }
-
-            // x = f(y_1, y_2, ..)
-            Instruction::Call(Call {
                 function,
                 arguments,
-                dest,
-                ..
-            }) => self.handle_call(function, arguments, dest.as_ref(), context),
+            } => {
+                let summary = &self.summaries[function];
+                for (arg, &param) in arguments
+                    .iter()
+                    .zip(&summary.parameters)
+                    .filter_map(|(a, p)| a.map(|a| (a, p)))
+                {
+                    let c = Constraint::Subset {
+                        left: Cell::new_local(arg, context.fun_name),
+                        right: Cell::new_local(param, function),
+                    };
+                    self.solver.add_constraint(c);
+                }
 
-            _ => {}
-        }
-    }
-
-    fn handle_terminator(&mut self, term: &Terminator, context: Context) {
-        match term {
-            Terminator::Invoke(Invoke {
-                function,
-                arguments,
-                result,
-                ..
-            }) => self.handle_call(function, arguments, Some(result), context),
+                match (summary.return_reg, dest) {
+                    (Some(return_name), Some(dest_name)) => {
+                        let c = Constraint::Subset {
+                            left: Cell::new_local(return_name, function),
+                            right: Cell::new_local(dest_name, context.fun_name),
+                        };
+                        self.solver.add_constraint(c);
+                    }
+                    _ => {}
+                }
+            }
 
             _ => {}
         }
--- src/module_visitor.rs
+++ src/module_visitor.rs
@@ -3,7 +3,7 @@ use llvm_ir::function::ParameterAttribute;
 use llvm_ir::instruction::{
     AddrSpaceCast, Alloca, BitCast, Call, InlineAssembly, Load, Phi, Store,
 };
-use llvm_ir::terminator::Invoke;
+use llvm_ir::terminator::{Invoke, Ret};
 use llvm_ir::{
     BasicBlock, Constant, Function, Instruction, Module, Name, Operand, Terminator, Type, TypeRef,
 };
@@ -17,12 +17,12 @@ pub struct Context<'a> {
 
 /// This trait allows implementors to define the `handle_instruction` and `handle_terminator` functions,
 /// which the `visit_module` function will call on all instructions and terminators.
-pub trait ModuleVisitor {
-    fn handle_function(&mut self, function: &Function, module: &Module);
-    fn handle_instruction(&mut self, instr: &Instruction, context: Context);
-    fn handle_terminator(&mut self, term: &Terminator, context: Context);
+pub trait ModuleVisitor<'a> {
+    fn handle_function(&mut self, function: &'a Function, module: &'a Module);
+    fn handle_instruction(&mut self, instr: &'a Instruction, context: Context<'a>);
+    fn handle_terminator(&mut self, term: &'a Terminator, context: Context<'a>);
 
-    fn visit_module(&mut self, module: &Module) {
+    fn visit_module(&mut self, module: &'a Module) {
         for function in &module.functions {
             self.handle_function(function, module);
             for block in &function.basic_blocks {
@@ -61,22 +61,28 @@ pub enum PointerInstruction<'a> {
         function: &'a str,
         arguments: Vec<Option<&'a Name>>,
     },
+    /// return x
+    Return { return_reg: &'a Name },
 }
 
 pub struct PointerContext<'a> {
-    function: &'a str,
+    pub fun_name: &'a str,
 }
 
-pub trait PointerVisitor {
-    fn handle_ptr_function(&mut self, name: &str, parameters: Vec<&Name>);
-    fn handle_ptr_instruction(&mut self, instr: PointerInstruction, context: PointerContext);
+pub trait PointerModuleVisitor<'a> {
+    fn handle_ptr_function(&mut self, name: &'a str, parameters: Vec<&'a Name>);
+    fn handle_ptr_instruction(
+        &mut self,
+        instr: PointerInstruction<'a>,
+        context: PointerContext<'a>,
+    );
 
     fn handle_call(
         &mut self,
-        function: &Either<InlineAssembly, Operand>,
-        arguments: &[(Operand, Vec<ParameterAttribute>)],
-        dest: Option<&Name>,
-        caller: &str,
+        function: &'a Either<InlineAssembly, Operand>,
+        arguments: &'a [(Operand, Vec<ParameterAttribute>)],
+        dest: Option<&'a Name>,
+        caller: &'a str,
     ) {
         // TODO: Filter out irrelevant function calls
         let (function, ty) = match function {
@@ -105,23 +111,29 @@ pub trait PointerVisitor {
             function,
             arguments,
         };
-        let context = PointerContext { function: caller };
+        let context = PointerContext { fun_name: caller };
         self.handle_ptr_instruction(instr, context);
     }
 }
 
-impl<T: PointerVisitor> ModuleVisitor for T {
-    fn handle_function(&mut self, function: &Function, _module: &Module) {
+impl<'a, T: PointerModuleVisitor<'a>> ModuleVisitor<'a> for T {
+    fn handle_function(&mut self, function: &'a Function, _module: &'a Module) {
         let parameters = function.parameters.iter().map(|p| &p.name).collect();
         self.handle_ptr_function(&function.name, parameters)
     }
 
-    fn handle_instruction(&mut self, instr: &Instruction, Context { function, .. }: Context) {
+    fn handle_instruction(
+        &mut self,
+        instr: &'a Instruction,
+        Context { function, .. }: Context<'a>,
+    ) {
         match instr {
+            // Instruction::ExtractElement(_) => todo!(),
+            // Instruction::ExtractValue(_) => todo!(),
             Instruction::Alloca(Alloca { dest, .. }) => {
                 let instr = PointerInstruction::Alloca { dest };
                 let context = PointerContext {
-                    function: &function.name,
+                    fun_name: &function.name,
                 };
                 self.handle_ptr_instruction(instr, context);
             }
@@ -141,12 +153,14 @@ impl<T: PointerVisitor> ModuleVisitor for T {
                 if let Some((value, _)) = get_operand_name_type(operand) {
                     let instr = PointerInstruction::Assign { dest, value };
                     let context = PointerContext {
-                        function: &function.name,
+                        fun_name: &function.name,
                     };
                     self.handle_ptr_instruction(instr, context);
                 };
             }
 
+            // Instruction::GetElementPtr(_) => todo!(),
+            // Instruction::IntToPtr(_) => todo!(),
             Instruction::Phi(Phi {
                 incoming_values,
                 dest,
@@ -164,7 +178,7 @@ impl<T: PointerVisitor> ModuleVisitor for T {
                     incoming_values,
                 };
                 let context = PointerContext {
-                    function: &function.name,
+                    fun_name: &function.name,
                 };
                 self.handle_ptr_instruction(instr, context);
             }
@@ -173,7 +187,7 @@ impl<T: PointerVisitor> ModuleVisitor for T {
                 Some((address, address_ty)) if is_ptr_ptr_type(address_ty) => {
                     let instr = PointerInstruction::Load { dest, address };
                     let context = PointerContext {
-                        function: &function.name,
+                        fun_name: &function.name,
                     };
                     self.handle_ptr_instruction(instr, context);
                 }
@@ -189,12 +203,14 @@ impl<T: PointerVisitor> ModuleVisitor for T {
                 if let Some((address, _)) = get_operand_name_type(address) {
                     let instr = PointerInstruction::Store { address, value };
                     let context = PointerContext {
-                        function: &function.name,
+                        fun_name: &function.name,
                     };
                     self.handle_ptr_instruction(instr, context);
                 }
             }
 
+            // Instruction::Select(_) => todo!(),
+            // Instruction::Freeze(_) => todo!(),
             Instruction::Call(Call {
                 function: callee,
                 arguments,
@@ -202,16 +218,18 @@ impl<T: PointerVisitor> ModuleVisitor for T {
                 ..
             }) => self.handle_call(callee, arguments, dest.as_ref(), &function.name),
 
+            // Instruction::VAArg(_) => todo!(),
             _ => {}
         }
     }
 
     fn handle_terminator(
         &mut self,
-        term: &Terminator,
+        term: &'a Terminator,
         Context {
-            function: caller, ..
-        }: Context,
+            function: context_fun,
+            ..
+        }: Context<'a>,
     ) {
         match term {
             Terminator::Invoke(Invoke {
@@ -219,7 +237,20 @@ impl<T: PointerVisitor> ModuleVisitor for T {
                 arguments,
                 result,
                 ..
-            }) => self.handle_call(function, arguments, Some(result), &caller.name),
+            }) => self.handle_call(function, arguments, Some(result), &context_fun.name),
+
+            Terminator::Ret(Ret { return_operand, .. }) => {
+                let return_reg = match return_operand.as_ref().and_then(get_operand_name_type) {
+                    Some((name, ty)) if is_ptr_type(ty) => name,
+                    _ => return,
+                };
+
+                let instr = PointerInstruction::Return { return_reg };
+                let context = PointerContext {
+                    fun_name: &context_fun.name,
+                };
+                self.handle_ptr_instruction(instr, context);
+            }
 
             _ => {}
         }
