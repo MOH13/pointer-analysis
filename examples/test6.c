5fac902abf2fdf32b8898870876ccc43141cbabe
Lasse Møldrup <lasse.moeldrup@gmail.com>
2023-03-11 19:30:04 +0100

    Refactors solver and adds test
    Co-authored-by: MOH13 <MOH13@users.noreply.github.com>

---
+++ examples/test6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+typedef struct my_struct
+{
+    int *f1;
+    int f2;
+} my_struct_t;
+
+int main()
+{
+    my_struct_t a;
+    my_struct_t *p = &a;
+    int x = 1;
+    p->f1 = &x;
+    int *q = &p->f2;
+}
