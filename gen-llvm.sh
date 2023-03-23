a44f018c9ad01d14b2ce197f20ec586c4244a5d8
Mads Overgård Henningsen <mads.overgaard@live.dk>
2023-03-23 12:32:24 +0000

    Added multi-file analysis

--- gen-llvm.sh
+++ gen-llvm.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-clang -S -fno-discard-value-names -emit-llvm $1 -o source.ll
+cd llvmtemp
+rm ./*
+for file in "$@"; do
+    clang -c -fno-discard-value-names -emit-llvm "../$file" -S
+done
