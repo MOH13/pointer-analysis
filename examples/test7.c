8f123dbe597c6c413e065f571a08db83cac8c0e9
Lasse Møldrup <lasse.moeldrup@gmail.com>
2023-03-27 18:33:41 +0200

    WIP: Better struct support

---
+++ examples/test7.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+typedef struct
+{
+    int a;
+} st;
+
+int main()
+{
+    int *ints = malloc(sizeof(int) * 10);
+    int *i = &ints[4];
+    st *structs = malloc(sizeof(st) * 10);
+    st *s = &structs[4];
+}
--- gen-llvm.sh
+++ gen-llvm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd llvmtemp
-rm ./*
+mkdir llvmtemp 1>/dev/null 2>&1
+cd llvmtemp && rm ./*
 for file in "$@"; do
     clang -c -fno-discard-value-names -emit-llvm "../$file" -S
 done
