3feb9ab1668d8db60764c3e95daf60dcb58aa0c7
Mads Overgård Henningsen <mads.overgaard@live.dk>
2023-04-13 08:57:30 +0000

    Refactored shell scripts for compilation

--- analyze.sh
+++ analyze.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 mkdir llvmtemp 1>/dev/null 2>&1
-cd llvmtemp && rm ./* 1>/dev/null 2>&1
+rm llvmtemp/* 1>/dev/null 2>&1
 for file in "$@"; do
-    clang -c -fno-discard-value-names -emit-llvm "../$file"
+    ./ctobc.sh $file "llvmtemp/$(basename -s .c $file).bc"
 done
-
-llvm-link-14 ./*.bc -o linked.ll -S
-llvm-link-14 ./*.bc -o linked.bc
-
-cd ..
+llvm-link-14 llvmtemp/*.bc -o llvmtemp/linked.ll -S
+llvm-link-14 llvmtemp/*.bc -o llvmtemp/linked.bc
 
 cargo run --release ./llvmtemp/linked.bc
